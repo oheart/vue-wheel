c73a9ff6a7e25c95681c7adc834c872a5ee87fa8
2353769123@qq.com <2353769123@qq.com>
2018-11-19 22:05:01 +0800

    添加icon和iconPos属性

--- README.md
+++ README.md
@@ -17,7 +17,10 @@ Learning to make vue wheels
 - [官方推荐](https://cn.vuejs.org/v2/guide/installation.html#Parcel)
 - [查看兼容性](https://caniuse.com/)
 
-3. 其他
+3. 图标
+- [iconfont](http://www.iconfont.cn/home/index?spm=a313x.7781069.1998910419.2.WeXcRo)
+- 图标使用方式： symbol引用
+4. 其他
 **git-open使用**
 - npm install -g git-open
 - 切换到项目目录，命令行输入git-open可打开项目远程所在的git仓库
--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,18 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPos}`]:true}">
+      <svg v-if="icon" class="icon">
+					<use :xlink:href=`#icon-${icon}`></use>
+			</svg>
+      <div class="content">
+         <slot></slot>
+      </div>
+    </button>
 </template>
 
 <script>
-export default {}
+export default {
+  props: ['icon','iconPos']
+}
 </script>
 
 <style lang="scss">
@@ -14,11 +23,32 @@ export default {}
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
   &:active {
     background-color: var(--button-active-bg);
+  }
+  > .icon{
+    order: 1;
+    margin-right: 0.1em;
+  }
+  > .content{
+    order: 2;
+  }
+  &.icon-right{
+     > .content{
+      order: 1
+    }
+    > .icon{
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
+    }
   }
 }
 </style>
