1d16aaca4bc151adfff4f714153669c0a0f9c108
2353769123@qq.com <2353769123@qq.com>
2018-11-19 23:21:23 +0800

    添加loading状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPos}`]:true}">
       <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+      <g-icon name="loading" class="loading"></g-icon>
       <div class="content">
          <slot></slot>
       </div>
@@ -23,6 +24,14 @@ export default {
 </script>
 
 <style lang="scss">
+@keyframes spin{
+  0%{
+    transform: rotate(0deg);
+  }
+  100%{
+    transform: rotate(360deg);
+  }
+}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -56,6 +65,9 @@ export default {
       margin-right: 0;
       margin-left: 0.1em;
     }
+  }
+  .loading{
+      animation: spin 2s infinite linear;
   }
 }
 </style>
