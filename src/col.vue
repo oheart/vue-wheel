cb18d2988792ce55510ccfaa3ae6e496f319dd58
2353769123@qq.com <2353769123@qq.com>
2019-02-28 22:47:43 +0800

    row和col基本实现

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+       :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}"> 
+       <div style="border: 1px solid red;height: 70px;">
+          <slot></slot>
+       </div>
     </div>
 </template>
 
@@ -10,7 +13,21 @@ export default{
     props:{
       span: {
         type: [Number, String]  // 接收Number和String两种形式 
-      } 
+      },
+      offset:{
+        type: [Number, String]
+      }
+    },
+    data(){
+      return {
+        gutter: 0
+      }
+    },
+    created(){
+       console.log('col created')
+    },
+    mounted(){
+      console.log('col mounted')
     }
 }
 </script>
@@ -18,9 +35,9 @@ export default{
 <style lang="scss" scoped>
 .col {
   width: 50%;
-  height: 100px;
-  background: #46A0FC;
-  border: 1px solid #f2df37;
+  height: 70px;
+  // background: #46A0FC;
+  // border: 1px solid #f2df37;
   color: #FFF;
 
   $class-prefix: col-;
@@ -29,5 +46,11 @@ export default{
         width: ($n / 24) * 100%;
     }
   } 
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+    }
+  }
 }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,41 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2 + 'px', marginRight: -gutter/2 + 'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default{
-     name: 'gRow'
+     name: 'gRow',
+     props: {
+         gutter:{
+             type: [Number, String]
+         }
+     },
+     created(){ // createElement
+        //  console.log(this.$children); // 没有儿子
+        console.log('row created')
+     },
+     mounted(){ // append到页面
+         console.log('row mounted')
+         console.log(this.$children); // 有儿子 
+         this.$children.forEach((vm) => {
+             vm.gutter = this.gutter;
+         })
+     }
+
+    /*
+        var div = document.createElement('div')   // created
+        var childDiv = document.createElement('div')   // child created
+        div.appendChild(childDiv)   // child mounted
+        document.body.appendChild(div)   // mounted
+    */
 }
 </script>
 
 <style  lang="scss" scoped>
 .row{
     display: flex;
+    // background: #9f9fea;
 }
 </style>
